0a207596bfe9bc7e162ad69f38feca5e80be0477
juliano rancoletta <julio45.jr@gmail.com>
2020-03-03 22:22:28 -0300

    Correção da Data

--- Teste.Business/Models/Usuario.cs
+++ Teste.Business/Models/Usuario.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using Teste.Business.Validation;
 
 namespace Teste.Business.Models
 {
@@ -11,7 +12,9 @@ namespace Teste.Business.Models
         public int id { get; set; }
         public string nome { get; set; }
         public string sobrenome { get; set; }
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
         public string Email { get; set; }
+        [Date(ErrorMessage = "data de nascimento não pode ser maior que Atual")]
         public DateTime DataNascimento { get; set; }
 
         public int IdEscolaridade { get; set; }
---
+++ Teste.Business/Validation/DateAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Teste.Business.Validation
+{
+    public class DateAttribute : RangeAttribute
+    {
+        public DateAttribute()
+          : base(typeof(DateTime),DateTime.MinValue.ToShortDateString(), DateTime.Now.ToShortDateString()) { }
+    }
+}
